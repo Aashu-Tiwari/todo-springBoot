1daa9e1aca10d13b7f7de7519b4f69107c8647e3
Aashu-Tiwari <ashutripathi573@gmail.com>
2025-08-04 15:08:58 +0530

    create and delete route is working

--- todo/src/main/java/com/app/todo/TodoApplication.java
+++ todo/src/main/java/com/app/todo/TodoApplication.java
@@ -1,14 +1,32 @@
 package com.app.todo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.app.todo.service.TaskService;
 
 @SpringBootApplication
 public class TodoApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner initData(TaskService taskService) {
+		return args -> {
+			// Add some sample tasks if the database is empty
+			if (taskService.getAllTasks().isEmpty()) {
+				taskService.createTask("Learn Spring Boot");
+
+				taskService.createTask("Build a Todo App");
+
+				taskService.createTask("Deploy to production");
+
+				System.out.println("Sample tasks initialized!");
+			}
+		};
+	}
 }
---
+++ todo/src/main/java/com/app/todo/controller/TaskRestController.java
@@ -0,0 +1,33 @@
+package com.app.todo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.todo.models.Task;
+import com.app.todo.service.TaskService;
+
+@RestController
+public class TaskRestController {
+    
+    private final TaskService taskService;
+
+    public TaskRestController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PutMapping("/tasks/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        Task updatedTask = taskService.updateTask(id, taskDetails);
+        return ResponseEntity.ok(updatedTask);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.ok().build();
+    }
+} 
